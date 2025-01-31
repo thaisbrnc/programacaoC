d727fa7e3a6437486be8834e5f14c35eac7bcf13
Thais Branco <133599028+thaisbrnc@users.noreply.github.com>
2025-01-31 00:15:02 +0000

    printf and scanf

--- olamundo.c
+++ olamundo.c
@@ -1,1 +1,21 @@
+#include <stdio.h>
 
+int main(){
+    int idade;
+    float altura;
+    char opcao;
+
+    printf("Olá, mundo!\n");
+
+
+    printf("Digite sua idade: \n");
+    scanf("%d", &idade);
+    printf("Digite sua altura: \n");
+    scanf("%f", &altura);
+    printf("Digite a opção: \n");
+    scanf(" %c", &opcao);
+
+    printf("\nIdade: %d\n", idade);
+    printf("Altura: %.2f\n", altura);
+    printf("Opção: %c\n\n", opcao);
+}
